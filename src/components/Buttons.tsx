7ae588b99f3ce2be949518531369290c25dca6f1
chloe <chloehuang170@gmail.com>
2022-09-20 08:56:20 +0800

    create Button components.

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  padding: 5px 16px;
+  font-weight: 500;
+  border-radius: 6px;
+  background-color: #f6f8fa;
+  color: #24292f;
+  border: 1px solid rgba(27, 31, 36, 0.15);
+`;
+
+export const PrimaryButton = styled(Button)`
+  background-color: #2da44e;
+  color: #fff;
+`;
--- src/pages/LabelManagement/LabelManagement.tsx
+++ src/pages/LabelManagement/LabelManagement.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { Button, PrimaryButton } from '../../components/Buttons';
+import styled from 'styled-components';
+
+const ButtonGroup = styled.div``;
+
+const Label = styled(Button)`
+  background-color: #0969da;
+  color: #fff;
+  border-radius: 6px 0 0 6px;
+`;
+
+const Milestones = styled(Button)`
+  border-radius: 0 6px 6px 0;
+`;
+
+const SearchInput = styled.input`
+  width: 320px;
+  border-radius: 6px;
+  background-color: #f6f8fa;
+  border: 1px solid rgba(27, 31, 36, 0.15);
+  padding: 5px 12px;
+  padding-left: 32px;
+`;
 
 const LabelManagement = () => {
-  return <div>LabelManagement</div>;
+  return (
+    <>
+      <ButtonGroup>
+        <Label>Labels</Label>
+        <Milestones>Milestones</Milestones>
+        <SearchInput placeholder="Search all labels" />
+      </ButtonGroup>
+      <PrimaryButton>New label</PrimaryButton>
+    </>
+  );
 };
 
 export default LabelManagement;
