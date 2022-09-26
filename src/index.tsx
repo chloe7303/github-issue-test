ba7128998aaf5c1d4f77e3727a1c134b20b38d5f
chloe <chloehuang170@gmail.com>
2022-09-26 16:26:03 +0800

    initialize TailwindCSS setting.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
