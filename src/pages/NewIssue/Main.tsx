510f92a3f7fb0063a1b23fbdf90699404725acfe
chloe <chloehuang170@gmail.com>
2022-10-07 16:53:55 +0800

    create markdown formatter and shortcuts handling.

--- src/pages/NewIssue/Main.tsx
+++ src/pages/NewIssue/Main.tsx
@@ -18,20 +18,24 @@ import {
   MarkdownIcon,
   InfoIcon,
 } from '@primer/octicons-react';
-import { useContext, useState } from 'react';
+import { useContext, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../components/buttons/Button';
 import { useCreateIssueMutation } from '../../redux/labelsApi';
 import { NewIssueContext, NewIssueContextType } from './NewIssue';
+import ReactMarkdown from 'react-markdown';
+import TextareaMarkdown, {
+  TextareaMarkdownRef,
+} from 'textarea-markdown-editor';
 
 const Main = () => {
-  const navigate = useNavigate();
   const { issueForm, setIssueForm } = useContext(
     NewIssueContext
   ) as NewIssueContextType;
   const [showPreview, setShowPreview] = useState(false);
-
+  const navigate = useNavigate();
   const [createIssue] = useCreateIssueMutation();
+  const ref = useRef<TextareaMarkdownRef>(null);
 
   const handleSubmit = async (issueForm) => {
     setIssueForm({
@@ -72,56 +76,122 @@ const Main = () => {
           <div className="mb-2 md:flex border-border lg:border-b border-solid justify-between flex-col lg:flex-row md:items-start lg:items-center">
             <div className="md:mt-2 md:mx-2 -mb-px flex">
               <button
-                className="py-3 px-4 md:rounded-t-[6px] border-border border border-solid md:mr-2 text-[14px] z-[1] border-b-[#fff] grow border-r-0 md:border-r"
+                className={`py-3 px-4 md:rounded-t-[6px] md:mr-2 text-[14px] z-[1] grow ${
+                  showPreview
+                    ? 'border-b-border'
+                    : 'border-b-[#fff] border-border border border-solid'
+                }`}
                 onClick={() => setShowPreview(false)}
               >
                 Write
               </button>
               <button
-                className="py-3 px-4 md:rounded-t-[6px] border-border border border-solid border-b-0 text-[14px] grow"
+                className={`py-3 px-4 md:rounded-t-[6px] text-[14px] grow ${
+                  showPreview
+                    ? 'border-b-[#fff] border-border border border-solid'
+                    : ''
+                }`}
                 onClick={() => setShowPreview(true)}
               >
                 Preview
               </button>
             </div>
-            <div className="text-muted flex justify-between border-border border-t border-solid w-full px-2 pt-2 lg:border-t-0 lg:justify-end">
+            <div
+              className={`text-muted flex justify-between border-border border-t border-solid w-full px-2 pt-2 lg:border-t-0 lg:justify-end ${
+                showPreview && 'hidden'
+              }`}
+            >
               <div className="md:hidden">
-                <TypographyIcon className="mr-1" />
-                <ChevronUpIcon />
+                <button className="mr-1 hover:text-emphasis">
+                  <TypographyIcon />
+                </button>
+                <button className="hover:text-emphasis">
+                  <ChevronUpIcon />
+                </button>
               </div>
               <div className="flex">
                 <div className="hidden md:block">
-                  <HeadingIcon className="ml-4" />
-                  <BoldIcon className="ml-4" />
-                  <ItalicIcon className="ml-4" />
+                  <button
+                    className="ml-4 hover:text-emphasis"
+                    onClick={() => ref.current?.trigger('h3')}
+                  >
+                    <HeadingIcon />
+                  </button>
+                  <button
+                    className="ml-4 hover:text-emphasis"
+                    onClick={() => ref.current?.trigger('bold')}
+                  >
+                    <BoldIcon />
+                  </button>
+                  <button
+                    className="ml-4 hover:text-emphasis"
+                    onClick={() => ref.current?.trigger('italic')}
+                  >
+                    <ItalicIcon />
+                  </button>
                 </div>
                 <div>
-                  <QuoteIcon className="ml-4" />
-                  <CodeIcon className="ml-4" />
-                  <LinkIcon className="ml-4" />
+                  <button
+                    className="ml-4 hover:text-emphasis"
+                    onClick={() => ref.current?.trigger('block-quotes')}
+                  >
+                    <QuoteIcon />
+                  </button>
+                  <button
+                    className="ml-4 hover:text-emphasis"
+                    onClick={() => ref.current?.trigger('code-inline')}
+                  >
+                    <CodeIcon />
+                  </button>
+                  <button
+                    className="ml-4 hover:text-emphasis"
+                    onClick={() => ref.current?.trigger('link')}
+                  >
+                    <LinkIcon />
+                  </button>
                 </div>
                 <div className="hidden md:block">
-                  <ListUnorderedIcon className="ml-4" />
-                  <ListOrderedIcon className="ml-4" />
-                  <TasklistIcon className="ml-4" />
+                  <button
+                    className="ml-4 hover:text-emphasis"
+                    onClick={() => ref.current?.trigger('unordered-list')}
+                  >
+                    <ListUnorderedIcon />
+                  </button>
+                  <button
+                    className="ml-4 hover:text-emphasis"
+                    onClick={() => ref.current?.trigger('ordered-list')}
+                  >
+                    <ListOrderedIcon />
+                  </button>
+                  <button className="ml-4 hover:text-emphasis ">
+                    <TasklistIcon />
+                  </button>
                 </div>
                 <div>
-                  <MentionIcon className="ml-4" />
-                  <span className="md:hidden">
-                    <ImageIcon className="ml-4" />
-                  </span>
-                  <CrossReferenceIcon className="ml-4" />
-                  <ReplyIcon className="ml-4" />
+                  <button className="ml-4 hover:text-emphasis">
+                    <MentionIcon />
+                  </button>
+                  <button className="md:hidden ml-4 hover:text-emphasis">
+                    <ImageIcon />
+                  </button>
+                  <button className="ml-4 hover:text-emphasis">
+                    <CrossReferenceIcon />
+                  </button>
+                  <button className="ml-4 hover:text-emphasis">
+                    <ReplyIcon />
+                  </button>
                 </div>
               </div>
             </div>
           </div>
           {showPreview ? (
-            <div>show pre</div>
+            <div className="m-2 p-2 max-w-none h-[230px] border-b-2 border-border border-solid prose">
+              <ReactMarkdown children={issueForm.body}></ReactMarkdown>
+            </div>
           ) : (
             <div className="m-2">
-              <textarea
-                className="rounded-md md:rounded-b-none text-[14px] text-text border-border border border-solid bg-default p-2 w-full h-[200px] md:border-b-0 align-top focus:bg-light focus-visible:outline-none focus-visible:border-emphasis focus-visible:border-2 focus-visible:border-b-0 peer"
+              <TextareaMarkdown
+                className="rounded-md md:rounded-b-none text-[14px] placeholder:text-text border-border border border-solid bg-default p-2 w-full h-[200px] md:border-b-0 align-top focus:bg-light focus-visible:outline-none focus-visible:border-emphasis focus-visible:border-2 focus-visible:border-b-0 peer leading-6"
                 placeholder="Leave a comment"
                 onChange={(e) =>
                   setIssueForm((prevValue) => ({
@@ -130,7 +200,15 @@ const Main = () => {
                   }))
                 }
                 value={issueForm.body}
-              ></textarea>
+                ref={ref}
+                options={{
+                  boldPlaceholder: '',
+                  headlinePlaceholder: '',
+                  italicPlaceholder: '',
+                  blockQuotesPlaceholder: '',
+                  linkTextPlaceholder: '',
+                }}
+              ></TextareaMarkdown>
               <div className="rounded-b-md text-[14px] border-border border-b border-x border-solid bg-default relative py-1.5 hidden md:block peer-focus:border-emphasis peer-focus:border-2 peer-focus:border-t-0">
                 <input type="file" className="w-full opacity-[.01]" />
                 <div className="flex justify-between border-t border-dashed border-border absolute top-0 pt-2 px-3 w-full text-text pointer-events-none">
