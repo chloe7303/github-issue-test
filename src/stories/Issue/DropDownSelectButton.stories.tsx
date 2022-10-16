6ffb84af30921234b7d0d9f3446826a57b0586f5
chloe <chloehuang170@gmail.com>
2022-10-16 19:29:24 +0800

    revise storybook component for interactivity.

--- src/stories/Issue/DropDownSelectButton.stories.tsx
+++ src/stories/Issue/DropDownSelectButton.stories.tsx
@@ -18,9 +18,6 @@ export default {
 } as ComponentMeta<typeof DropDownSelectButton>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof DropDownSelectButton> = (args) => (
-  <DropDownSelectButton {...args} />
-);
 
 export const CloseIssue = () => {
   const [selectedIssueAction, setSelectedIssueAction] = useState({
@@ -33,41 +30,95 @@ export const CloseIssue = () => {
   });
 
   return (
-    <DropDownSelectButton
-      actionType={[
-        {
-          title: 'Closed as completed',
-          description: 'Done, closed, fixed, resolved',
-          icon: <IssueClosedIcon fill="#8250df" />,
-          state_reason: 'completed',
-          handleClick: () =>
-            setSelectedIssueAction({
-              title: 'Close issue',
-              icon: <IssueClosedIcon fill="#8250df" />,
-              body: {
-                state: 'closed',
-                state_reason: 'completed',
-              },
-            }),
-        },
-        {
-          title: 'Closed as not planned',
-          description: "Won't fix, can't repro, duplicate, stale",
-          icon: <SkipIcon fill="#57606a" />,
-          state_reason: 'not_planned',
-          handleClick: () =>
-            setSelectedIssueAction({
-              title: 'Close issue',
-              icon: <SkipIcon fill="#57606a" />,
-              body: {
-                state: 'closed',
-                state_reason: 'not_planned',
-              },
-            }),
-        },
-      ]}
-      selectedIssueAction={selectedIssueAction}
-      handleSubmit={() => {}}
-    ></DropDownSelectButton>
+    <div className="pl-[200px]">
+      <DropDownSelectButton
+        actionType={[
+          {
+            title: 'Closed as completed',
+            description: 'Done, closed, fixed, resolved',
+            icon: <IssueClosedIcon fill="#8250df" />,
+            state_reason: 'completed',
+            handleClick: () =>
+              setSelectedIssueAction({
+                title: 'Close issue',
+                icon: <IssueClosedIcon fill="#8250df" />,
+                body: {
+                  state: 'closed',
+                  state_reason: 'completed',
+                },
+              }),
+          },
+          {
+            title: 'Closed as not planned',
+            description: "Won't fix, can't repro, duplicate, stale",
+            icon: <SkipIcon fill="#57606a" />,
+            state_reason: 'not_planned',
+            handleClick: () =>
+              setSelectedIssueAction({
+                title: 'Close issue',
+                icon: <SkipIcon fill="#57606a" />,
+                body: {
+                  state: 'closed',
+                  state_reason: 'not_planned',
+                },
+              }),
+          },
+        ]}
+        selectedIssueAction={selectedIssueAction}
+        handleSubmit={() => {}}
+      ></DropDownSelectButton>
+    </div>
+  );
+};
+
+export const ReopenIssue = () => {
+  const [selectedIssueAction, setSelectedIssueAction] = useState({
+    title: 'Reopen',
+    icon: <IssueReopenedIcon fill="#1a7f37" />,
+    body: {
+      state: 'open',
+      state_reason: 'reopened',
+    },
+  });
+
+  return (
+    <div className="pl-[200px]">
+      <DropDownSelectButton
+        actionType={[
+          {
+            title: 'Reopen',
+            description: '',
+            icon: <IssueReopenedIcon fill="#1a7f37" />,
+            state_reason: 'reopened',
+            handleClick: () =>
+              setSelectedIssueAction({
+                title: 'Reopen',
+                icon: <IssueReopenedIcon fill="#1a7f37" />,
+                body: {
+                  state: 'open',
+                  state_reason: 'reopened',
+                },
+              }),
+          },
+          {
+            title: 'Closed as not planned',
+            description: '',
+            icon: <SkipIcon fill="#57606a" />,
+            state_reason: 'not_planned',
+            handleClick: () =>
+              setSelectedIssueAction({
+                title: 'Close as not planned',
+                icon: <SkipIcon fill="#57606a" />,
+                body: {
+                  state: 'closed',
+                  state_reason: 'not_planned',
+                },
+              }),
+          },
+        ]}
+        selectedIssueAction={selectedIssueAction}
+        handleSubmit={() => {}}
+      ></DropDownSelectButton>
+    </div>
   );
 };
